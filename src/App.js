9f48a72641f001f4ab7c44ff883b1585e26bfece
riteshsutariya <riteshsutariya94@gmail.com>
2022-05-07 11:05:29 +0530

    commiting all files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import data from './data';
+/*import data from './data';
 import React,{useState} from 'react';
 import List from './List';
 
@@ -10,6 +10,28 @@ function App() {
         <h2>{people.length} Birthdays Today.</h2>
         <List people={people} />
         <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
+  )
+}
+export default App;
+*/
+
+
+import data from './data'
+import {useState} from 'react'
+import List from './List'
+
+function App() {
+  const [people, setPeople] = useState(data)
+  const [flag,setFlag]= useState(true)
+  return (
+    <main>
+      <section className='container'>
+        <h2>{people.length} Birthdays today.</h2>
+        <List people={people} />
+        <button disabled={!flag} onClick={()=>{setPeople([]);setFlag(false)}}>Clear all</button>
+        <button disabled={flag} onClick={()=>{setPeople(data);setFlag(true)}}>Get all</button>
       </section>
     </main>
   )
--- src/List.js
+++ src/List.js
@@ -19,3 +19,24 @@ const List=({ people })=>{
 }
 export default List;
 
+
+/*
+export default function List({people}){
+   return(
+       <>
+       {people.map((person)=>{
+           return(
+               <>
+               <article key={person.id} className='person'>
+                   <img src={person.image} alt={person.name} />
+                   <div>
+                   <h4>{person.name}</h4>
+                   <p>{person.age} years.</p>
+                   </div>
+               </article>
+               </>
+           )
+       })}
+       </>
+   )
+}*/
